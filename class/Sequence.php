dc975809a9855d541f13494c62fed132871e9b00
Petro Ostapuk <php8@gerchikco.com>
2019-05-01 16:06:27 +0300

    Added Sequence abstract class

---
+++ class/Sequence.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Class Sequence
+ */
+abstract class Sequence
+{
+    /**
+     * @param string $item
+     */
+    abstract public function put(string $item): void;
+
+    /**
+     * @return null|string
+     */
+    abstract public function get(): ?string;;
+
+    /**
+     * @return bool
+     */
+    abstract public function isEmpty(): bool;
+}
